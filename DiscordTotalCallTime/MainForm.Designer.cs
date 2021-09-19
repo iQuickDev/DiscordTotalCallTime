a9abe84d243737bf1943fcfcd966c1cd77cdff7b
iQuickGaming <iquickpublic@gmail.com>
2021-09-19 22:32:31 +0200

    very cool modern icon

--- DiscordTotalCallTime/MainForm.Designer.cs
+++ DiscordTotalCallTime/MainForm.Designer.cs
@@ -48,20 +48,20 @@ namespace DiscordTotalCallTime
             this.calls = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.operationProgress = new Guna.UI2.WinForms.Guna2CircleProgressBar();
             this.importJSON = new Guna.UI2.WinForms.Guna2Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.softwareIcon = new System.Windows.Forms.PictureBox();
             this.trustLabel = new System.Windows.Forms.Label();
             this.dragPanel.SuspendLayout();
             this.totalCallTimeContainer.SuspendLayout();
             this.avgCallTimeContainer.SuspendLayout();
             this.startedCallsContainer.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.startedCalls)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.softwareIcon)).BeginInit();
             this.SuspendLayout();
             // 
             // dragPanel
             // 
             this.dragPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(52)))), ((int)(((byte)(58)))));
-            this.dragPanel.Controls.Add(this.pictureBox1);
+            this.dragPanel.Controls.Add(this.softwareIcon);
             this.dragPanel.Controls.Add(this.closeBtn);
             this.dragPanel.Location = new System.Drawing.Point(0, 0);
             this.dragPanel.Name = "dragPanel";
@@ -369,15 +369,15 @@ namespace DiscordTotalCallTime
             this.importJSON.Text = "Import JSON";
             this.importJSON.UseTransparentBackground = true;
             // 
-            // pictureBox1
+            // softwareIcon
             // 
-            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(285, 0);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(30, 30);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 10;
-            this.pictureBox1.TabStop = false;
+            this.softwareIcon.Image = ((System.Drawing.Image)(resources.GetObject("softwareIcon.Image")));
+            this.softwareIcon.Location = new System.Drawing.Point(285, 0);
+            this.softwareIcon.Name = "softwareIcon";
+            this.softwareIcon.Size = new System.Drawing.Size(30, 30);
+            this.softwareIcon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.softwareIcon.TabIndex = 10;
+            this.softwareIcon.TabStop = false;
             // 
             // trustLabel
             // 
@@ -408,6 +408,7 @@ namespace DiscordTotalCallTime
             this.Controls.Add(this.confirmBtn);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "DiscordTotalCallTime";
             this.dragPanel.ResumeLayout(false);
@@ -417,7 +418,7 @@ namespace DiscordTotalCallTime
             this.avgCallTimeContainer.PerformLayout();
             this.startedCallsContainer.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.startedCalls)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.softwareIcon)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -440,7 +441,7 @@ namespace DiscordTotalCallTime
         private System.Windows.Forms.DataGridViewTextBoxColumn calls;
         private Guna.UI2.WinForms.Guna2CircleProgressBar operationProgress;
         private Guna.UI2.WinForms.Guna2Button importJSON;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox softwareIcon;
         private System.Windows.Forms.Label trustLabel;
     }
 }
